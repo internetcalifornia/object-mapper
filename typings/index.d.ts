14c5692cc8e0b69acffcc64c213ebb37863d8fcd
Scott Eremia-Roden <scott.eremia-roden@nems.org>
2021-11-21 19:24:31 -0800

    changed type definition

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,13 +1,13 @@
 type syncMap<P, U> = (data: U) => P;
 type asyncMap<P, U> = (data: U) => Promise<P>;
 
-export type MappingDefinition<T = any, U = any> = {
+export type MappingDefinition<T extends object = any, U> = {
   [P in keyof T]:
     | { map: syncMap<T[P], U>; asyncMap?: never }
     | { map?: never; asyncMap: asyncMap<T[P], U> };
 };
 
-export type MappingFunc<T = any, U = any> = (
+export type MappingFunc<T, U extends object> = (
   definition: MappingDefinition<T, U>,
   data: U,
   options?: { strict?: boolean }
