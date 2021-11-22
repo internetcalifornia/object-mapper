b6d9a104fba1db53a94d286c438ddf0cec5f8b5e
Scott Eremia-Roden <scott.eremia-roden@nems.org>
2021-11-21 19:47:24 -0800

    removed lib

--- lib/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { MappingDefinition } from "../typings";
-export declare abstract class ObjectMapper {
-    static map<T extends object, P = any>(data: P, definition: MappingDefinition<T, typeof data>, options?: {
-        strict?: boolean;
-    }): Promise<T>;
-}
-export default ObjectMapper;
