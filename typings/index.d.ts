a10b8f258ad6ca06260ef0499272cc4d86d9b9a3
Scott Eremia-Roden <scott.eremia-roden@nems.org>
2021-12-22 11:50:53 -0800

    update type

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -2,9 +2,17 @@ export type MappingDefinition<T extends object = any, U = any> = {
   [P in keyof T]:
     | { map: (data: U) => P; asyncMap?: never }
     | { map?: never; asyncMap: (data: U) => Promise<T[P]> }
-    | ((data: U) => T[P]);
+    | ((data: U) => T[P])
+    | string
+    | boolean
+    | number;
 };
 
 export type MappingDefinitionSync<T extends object = any, U = any> = {
-  [P in keyof T]: { map: (data: U) => T[P] } | ((data: U) => T[P]);
+  [P in keyof T]:
+    | { map: (data: U) => T[P] }
+    | ((data: U) => T[P])
+    | string
+    | boolean
+    | number;
 };
